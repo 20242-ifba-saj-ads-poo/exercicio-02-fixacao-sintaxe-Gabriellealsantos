085370635f777b7fb6354b442206bc4e26d15fb0
gabriel <gabriel.lealsantos@hotmail.com>
2024-11-14 14:38:00 -0300

    Atividade 4

--- fatorial/src/application/Program.java
+++ fatorial/src/application/Program.java
@@ -1,15 +1,27 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
-		for (int i = 1; i <= 100; i++) {
-			if (i % 3 == 0) {
-				System.out.println(i);
-			}
-		}
+        System.out.println("Digite um número para calcular o fatorial:");
+        int numero = sc.nextInt();
 
-	}
+        int fatorial = 1;
+
+        if (numero == 0) {
+            System.out.println("O fatorial de 0 é 1");
+        } else {
+            for (int i = 1; i <= numero; i++) {
+                fatorial = fatorial * i;
+                System.out.printf("O fatorial de %d é (%d!) * %d = %d \n", i, i - 1, i, fatorial);
+            }
+        }
+
+        sc.close();
+    }
 }
