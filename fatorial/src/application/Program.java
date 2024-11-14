3abb9e1310289fd1d2c7fe40445e7209f1558e2e
gabriel <gabriel.lealsantos@hotmail.com>
2024-11-14 14:45:51 -0300

    Fatorial com Long

--- fatorial/src/application/Program.java
+++ fatorial/src/application/Program.java
@@ -4,24 +4,29 @@ import java.util.Scanner;
 
 public class Program {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite um número para calcular o fatorial:");
-        int numero = sc.nextInt();
+		/*
+		 * System.out.println("Digite um número para calcular o fatorial:"); int numero
+		 * = sc.nextInt();
+		 * 
+		 * int fatorial = 1;
+		 * 
+		 * if (numero == 0) { System.out.println("O fatorial de 0 é 1"); } else { for
+		 * (int i = 1; i <= numero; i++) { fatorial = fatorial * i;
+		 * System.out.printf("O fatorial de %d é (%d!) * %d = %d \n", i, i - 1, i,
+		 * fatorial); } }
+		 */
 
-        int fatorial = 1;
+		long fatorial = 1;
 
-        if (numero == 0) {
-            System.out.println("O fatorial de 0 é 1");
-        } else {
-            for (int i = 1; i <= numero; i++) {
-                fatorial = fatorial * i;
-                System.out.printf("O fatorial de %d é (%d!) * %d = %d \n", i, i - 1, i, fatorial);
-            }
-        }
+		for (int i = 1; i <= 40; i++) {
+			fatorial = fatorial * i;
+			System.out.printf("O fatorial de %d é (%d!) * %d = %d \n", i, i - 1, i, fatorial);
+		}
 
-        sc.close();
-    }
+		sc.close();
+	}
 }
