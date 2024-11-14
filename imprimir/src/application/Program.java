9d65cb2d5d386287b38de2a3e665dd5241fe41b0
gabriel <gabriel.lealsantos@hotmail.com>
2024-11-14 13:28:53 -0300

    Atividade 1

---
+++ imprimir/src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int N = 150;
+		
+		for(; N < 300;) {
+			N += 1;
+			System.out.println(N);
+		}
+
+	}
+
+}
