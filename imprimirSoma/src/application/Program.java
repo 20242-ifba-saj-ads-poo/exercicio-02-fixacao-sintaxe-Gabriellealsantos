5ec8ff6ef30806166f2b8a29b92900cf90350159
gabriel <gabriel.lealsantos@hotmail.com>
2024-11-14 13:48:46 -0300

    Atividade 2

---
+++ imprimirSoma/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program {
+
+    public static void main(String[] args) {
+    	
+        int soma = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+            soma += i;
+        }
+
+        System.out.println(soma);
+    }
+}
